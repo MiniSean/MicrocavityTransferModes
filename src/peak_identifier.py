bd5777ccbf6e533425254302e47f6429f952a9c2
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-11-26 16:19:00 +0100

    Updated now utilizing scipy's peak prominence metric

--- src/peak_identifier.py
+++ src/peak_identifier.py
@@ -1,6 +1,6 @@
 import numpy as np
-from scipy.signal import argrelextrema
-from typing import List
+from scipy.signal import find_peaks, peak_prominences
+from typing import List, Tuple, Dict
 
 
 class PeakData:
@@ -8,9 +8,20 @@ class PeakData:
         pass
 
 
-def identify_peaks(meas_data: np.ndarray):  # -> List[PeakData]:
-    # np.r_[True, a[1:] < a[:-1]] & numpy.r_[a[:-1] < a[1:], True]
-    return argrelextrema(meas_data, np.less)
+def identify_peaks(meas_data: np.ndarray) -> Tuple[np.ndarray, Dict]:  # -> List[PeakData]:
+    mean_prominence = np.mean(identify_peak_prominence(meas_data)[0])  # Average peak prominence
+    return find_peaks(x=meas_data, height=identify_noise_ceiling(meas_data), prominence=mean_prominence, distance=10)  # Arbitrary distance value
+
+
+def identify_noise_ceiling(meas_data: np.ndarray) -> float:
+    mean = np.mean(meas_data)
+    std = np.std(meas_data)
+    return mean + .1 * std
+
+
+def identify_peak_prominence(meas_data: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    all_peaks_above_noise = find_peaks(x=meas_data, height=identify_noise_ceiling(meas_data))[0]
+    return peak_prominences(x=meas_data, peaks=all_peaks_above_noise)
 
 
 if __name__ == '__main__':
@@ -22,17 +33,22 @@ if __name__ == '__main__':
     slice = (1050000, 1150000)
     measurement_class.slicer = slice  # Zooms in on relevant data part
 
+    # Collect noise ground level
+    noise_ceiling = identify_noise_ceiling(measurement_class.y_data)
     # Collect peaks
     peak_indices = identify_peaks(measurement_class.y_data)
 
     # Apply axis draw/modification
     ax = plot_class(axis=ax, measurement_class=measurement_class)
 
+    # Draw noise h-lines
+    ax.axhline(y=noise_ceiling, color='r')
     # Draw peak v-lines
-    # for i, index in enumerate(peak_indices[0]):
-    #     if i > 100:
-    #         break
-    #     ax.axvline(x=index)
+    print(len(peak_indices[0]))
+    for i, index in enumerate(peak_indices[0]):
+        if i > 1000:  # safety break
+            break
+        ax.axvline(x=measurement_class.x_data[index], color='r', alpha=0.5)  # ymax=measurement_class.y_data[index],
 
     # Show
     plt.show()
