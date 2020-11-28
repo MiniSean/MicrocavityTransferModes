83494b2830b3168b00304d22d6bcf3eb0e241d28
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-11-28 19:52:30 +0100

    Updated measurement plot to draw different iterations

--- src/plot_npy.py
+++ src/plot_npy.py
@@ -36,11 +36,11 @@ def plot_npy(axis: plt.axes, measurement_file: str, sample_file: str, slice: Tup
     return axis
 
 
-def prepare_measurement_plot() -> Tuple[plt.axes, SyncMeasData]:
+def prepare_measurement_plot(measure_file: str = 'transrefl_hene_1s_10V_PMT5_rate1300000.0itteration0') -> Tuple[plt.axes, SyncMeasData]:
     # Allows to surpass the hardcoded limit in the number of points in the backend Agg
     rcParams['agg.path.chunksize'] = 1000
     # Define file name to retrieve from predefined data path
-    file_meas = 'transrefl_hene_1s_10V_PMT5_rate1300000.0itteration0'
+    file_meas = measure_file
     file_samp = 'samples_1s_10V_rate1300000.0'
     # Store plot figure and axis
     _, result_axis = plt.subplots()
