7f718c87537d354761af3f223b99f2dceee62407
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-11-28 19:53:14 +0100

    Updated PeakData class. Added PeakCollection class

--- src/peak_identifier.py
+++ src/peak_identifier.py
@@ -1,22 +1,62 @@
 import numpy as np
 from scipy.signal import find_peaks, peak_prominences
 from typing import List, Tuple, Dict
+from src.import_data import SyncMeasData
 
 
-class PeakData:
-    def __init__(self):
-        pass
+class PeakData(float):
+
+    def __new__(cls, data: SyncMeasData, index: int):
+        value = data.y_data[index]
+        return float.__new__(cls, value)
+
+    def __init__(self, data: SyncMeasData, index: int):
+        # (self, loc: float, height: float)
+        self._data = data
+        self._index_pointer = index
+        super(float).__init__()
+
+    @property
+    def get_x(self):
+        return self._data.x_data[self._index_pointer]
+
+    @property
+    def get_y(self):
+        return self._data.y_data[self._index_pointer]
+
+    def get_x_offset(self, offset_index: int):
+        return self._data.x_data[self._index_pointer + offset_index]
 
 
-def identify_peaks(meas_data: np.ndarray) -> Tuple[np.ndarray, Dict]:  # -> List[PeakData]:
-    mean_prominence = np.mean(identify_peak_prominence(meas_data)[0])  # Average peak prominence
-    return find_peaks(x=meas_data, height=identify_noise_ceiling(meas_data), prominence=mean_prominence, distance=10)  # Arbitrary distance value
+class PeakCollection:
+    def __init__(self, collection: List[PeakData]):
+        self._list = collection
+        self._global_offset = 0  # Temp
+
+    def __iter__(self):
+        return self._list.__iter__()
+
+    def __len__(self):
+        return self._list.__len__()
+
+    def __getitem__(self, item):
+        return self._list.__getitem__(item)
+
+    def index(self, *args, **kwargs):
+        return self._list.index(*args, **kwargs)
+
+
+def identify_peaks(meas_data: SyncMeasData) -> PeakCollection:
+    mean_prominence = np.mean(identify_peak_prominence(meas_data.y_data)[0])  # Average peak prominence
+    peak_indices, properties = find_peaks(x=meas_data.y_data, height=identify_noise_ceiling(meas_data.y_data), prominence=mean_prominence, distance=10)  # Arbitrary distance value
+    peak_collection = PeakCollection([PeakData(data=meas_data, index=i) for i in peak_indices])
+    return peak_collection
 
 
 def identify_noise_ceiling(meas_data: np.ndarray) -> float:
     mean = np.mean(meas_data)
     std = np.std(meas_data)
-    return mean + .1 * std  #TODO: still hardcoded. Need to find a satisfying way of representing noise ceiling
+    return mean + .1 * std  # TODO: still hardcoded. Need to find a satisfying way of representing noise ceiling
 
 
 def identify_peak_prominence(meas_data: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
@@ -28,7 +68,7 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from src.plot_npy import prepare_measurement_plot, plot_class
     # Construct measurement class
-    ax, measurement_class = prepare_measurement_plot()
+    ax, measurement_class = prepare_measurement_plot('transrefl_hene_1s_10V_PMT5_rate1300000.0itteration0')
     # Optional, define slice
     slice = (1050000, 1150000)
     measurement_class.slicer = slice  # Zooms in on relevant data part
@@ -36,7 +76,7 @@ if __name__ == '__main__':
     # Collect noise ground level
     noise_ceiling = identify_noise_ceiling(measurement_class.y_data)
     # Collect peaks
-    peak_indices = identify_peaks(measurement_class.y_data)
+    peak_collection = identify_peaks(measurement_class)
 
     # Apply axis draw/modification
     ax = plot_class(axis=ax, measurement_class=measurement_class)
@@ -44,11 +84,11 @@ if __name__ == '__main__':
     # Draw noise h-lines
     ax.axhline(y=noise_ceiling, color='r')
     # Draw peak v-lines
-    print(len(peak_indices[0]))
-    for i, index in enumerate(peak_indices[0]):
+    print(len(peak_collection))
+    for i, peak_data in enumerate(peak_collection):
         if i > 1000:  # safety break
             break
-        ax.axvline(x=measurement_class.x_data[index], color='r', alpha=0.5)  # ymax=measurement_class.y_data[index],
+        ax.axvline(x=peak_data.get_x, ymax=peak_data.get_y, color='r', alpha=1)
 
     # Show
     plt.show()
