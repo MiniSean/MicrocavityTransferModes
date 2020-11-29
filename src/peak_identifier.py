029c00fd8683bc1860deb23455a5105dd6aff252
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-11-30 00:18:06 +0100

    Updated noise ceiling value

--- src/peak_identifier.py
+++ src/peak_identifier.py
@@ -73,7 +73,7 @@ def identify_peaks(meas_data: SyncMeasData) -> PeakCollection:
 def identify_noise_ceiling(meas_data: SyncMeasData) -> float:
     mean = np.mean(meas_data.data_array[0])
     std = np.std(meas_data.data_array[0])
-    return mean + .21 * std  # TODO: still hardcoded. Need to find a satisfying way of representing noise ceiling
+    return mean + .24 * std  # TODO: still hardcoded. Need to find a satisfying way of representing noise ceiling
 
 
 def identify_peak_prominence(meas_data: SyncMeasData) -> Tuple[np.ndarray, np.ndarray]:
