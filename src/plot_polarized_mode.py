Fixed minor bug (do not use measurement data slicing mid calculation)

src/plot_polarized_mode.py
@@ -1,6 +1,7 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import logging
-from src.plot_npy import get_standard_axis, plot_class, plot_peak_collection
+from src.plot_npy import get_standard_axis, plot_class, plot_peak_collection, plot_cluster_collection
 from src.import_data import SyncMeasData
 from src.peak_relation import LabeledPeakCollection, get_slice, flatten_clusters
 
@@ -13,24 +14,19 @@ def plot_isolated_long_mode(axis: plt.axes, data_class: SyncMeasData, collection
         logging.warning(f'Longitudinal mode {long_mode} not well defined')
         return axis
 
-    data_slice = get_slice(data_class=data_class, value_slice=value_slice)
-    # Update measurement data indices (slice)
-    data_class.slicer = data_slice
-    # Update peak indices
-    peak_array = []
-    for peak in flatten_clusters(data=cluster_array):
-        if peak.update_index() is not None:
-            peak_array.append(peak)
-
+    axis.set_xlim(value_slice)
     # Plot data (slice)
     axis = plot_class(axis=axis, measurement_class=data_class)
-    # Plot peak array
-    axis = plot_peak_collection(axis=axis, data=peak_array)
+    # Plot cluster array
+    axis = plot_cluster_collection(axis=axis, data=cluster_array)
+    # # Plot peak array
+    # axis = plot_peak_collection(axis=axis, data=flatten_clusters(data=cluster_array))
     return get_standard_axis(axis=axis)
 
 
 if __name__ == '__main__':
     from src.peak_identifier import identify_peaks
+    from src.plot_npy import prepare_measurement_plot
 
     # Reference files
     file_samp = 'samples_1s_10V_rate1300000.0'
@@ -42,11 +38,25 @@ if __name__ == '__main__':
     identified_peaks = [identify_peaks(meas_data=data) for data in meas_iterations]
     labeled_peaks = [LabeledPeakCollection(optical_mode_collection=collection) for collection in identified_peaks]
 
-
-
     # Test
     index = 0
-    for long_mode in range(3):
-        fig, ax = plt.subplots()
-        plot_isolated_long_mode(axis=ax, data_class=meas_iterations[index], collection=labeled_peaks[index], long_mode=long_mode)
+
+    ax_full, measurement_class = prepare_measurement_plot(filenames[index])
+    ax_full = plot_class(axis=ax_full, measurement_class=measurement_class)
+
+    fig, ax_array = plt.subplots(2, 2)
+    ax_array = np.ndarray.flatten(ax_array)
+    for long_mode in range(len(ax_array)):
+        # Plot specific longitudinal mode
+        plot_isolated_long_mode(axis=ax_array[long_mode], data_class=meas_iterations[index], collection=labeled_peaks[index], long_mode=long_mode)
+        if long_mode == 1:
+            ax_array[long_mode].legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+
+        # Plot mode separation
+        cluster_array, value_slice = labeled_peaks[index].get_mode_sequence(long_mode=long_mode)
+        # Temporary coloring
+        ax_full.axvline(x=value_slice[0], color=f'C{long_mode+1}')
+        ax_full.axvline(x=value_slice[1], color=f'C{long_mode+1}', label=f'Long. Mode={long_mode}')
+
+    ax_full.legend()
     plt.show()