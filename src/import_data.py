fbf0f9cf87c892a95f4ddaf73203c3451d93e34d
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-11-30 14:18:35 +0100

    Added slice getter. Added index within slice range check

--- src/import_data.py
+++ src/import_data.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 # import logging
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Union
 DATA_DIR = 'data/Trans'
 
 
@@ -19,26 +19,34 @@ class SyncMeasData:
         self._slicer = None
 
     # Getters
-    def get_transmitted_data(self):
+    def get_transmitted_data(self) -> np.ndarray:
         return self.get_sliced_array(self.data_array[0])
 
-    def get_sample_data(self):
+    def get_sample_data(self) -> np.ndarray:
         return self.get_sliced_array(self.samp_array)
+
+    def get_slicer(self) -> Union[None, Tuple[int, int]]:
+        return self._slicer
 
     # Setters
     def set_slicer(self, new_slicer: Tuple[int, int]):
         self._slicer = new_slicer
+        if self._slicer[0] > self._slicer[1]:  # Swap slice order to maintain (low, high) bound structure
+            self._slicer = (self._slicer[1], self._slicer[0])
 
     # Properties
     y_data = property(get_transmitted_data)
     x_data = property(get_sample_data)
-    slicer = property(None, set_slicer)
+    slicer = property(get_slicer, set_slicer)
 
     def get_sliced_array(self, array: np.ndarray):
         try:
             return slice_array(array=array, slice=self._slicer)
         except TypeError:
             return array
+
+    def inside_slice_range(self, index: int) -> bool:
+        return (0 <= index) and (index <= self.slicer[1] - self.slicer[0])
 
 
 def import_npy(filename: str) -> np.ndarray:
