245aed380506bed337a407e7d4bc9f015d2c697e
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-12-02 00:06:34 +0100

    Updated isolated mode plot to also zoom on single transverse modes

--- src/plot_polarized_mode.py
+++ src/plot_polarized_mode.py
@@ -1,27 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
+from typing import List, Union
 from src.plot_npy import get_standard_axis, plot_class, plot_peak_collection, plot_cluster_collection
 from src.import_data import SyncMeasData
-from src.peak_relation import LabeledPeakCollection, get_slice, flatten_clusters
+from src.peak_relation import LabeledPeakCollection, get_value_to_data_slice, flatten_clusters
 
 
-def plot_isolated_long_mode(axis: plt.axes, data_class: SyncMeasData, collection: LabeledPeakCollection, long_mode: int) -> plt.axis:
+def plot_isolated_long_mode(axis: plt.axes, data_class: SyncMeasData, collection: LabeledPeakCollection, long_mode: int, trans_mode: Union[int, None]) -> plt.axis:
     # Plot array
     try:
-        cluster_array, value_slice = collection.get_mode_sequence(long_mode=long_mode)
+        cluster_array, value_slice = collection.get_mode_sequence(long_mode=long_mode, trans_mode=trans_mode)
     except ValueError:
         logging.warning(f'Longitudinal mode {long_mode} not well defined')
         return axis
 
-    axis.set_xlim(value_slice)
-    # Plot data (slice)
+    data_class.slicer = get_value_to_data_slice(data_class=data_class, value_slice=value_slice)
+    # Plot data (data_slice)
     axis = plot_class(axis=axis, measurement_class=data_class)
     # Plot cluster array
     axis = plot_cluster_collection(axis=axis, data=cluster_array)
     # # Plot peak array
     # axis = plot_peak_collection(axis=axis, data=flatten_clusters(data=cluster_array))
     return get_standard_axis(axis=axis)
+
+
+def plot_3d_sequence(axis: plt.axis, data_classes: List[SyncMeasData], long_mode: int, trans_mode: Union[int, None]) -> plt.axis:
+    # Set 3D plot
+    axis = plt.subplot(projection='3d')
+    # for i, data_class in enumerate(data_classes):
+    #     axis.plot(measurement_class.x_data, measurement_class.y_data)
+    return axis
 
 
 if __name__ == '__main__':
@@ -48,7 +57,7 @@ if __name__ == '__main__':
     ax_array = np.ndarray.flatten(ax_array)
     for long_mode in range(len(ax_array)):
         # Plot specific longitudinal mode
-        plot_isolated_long_mode(axis=ax_array[long_mode], data_class=meas_iterations[index], collection=labeled_peaks[index], long_mode=long_mode)
+        ax_array[long_mode] = plot_isolated_long_mode(axis=ax_array[long_mode], data_class=meas_iterations[index], collection=labeled_peaks[index], long_mode=long_mode, trans_mode=2)
         if long_mode == 1:
             ax_array[long_mode].legend(bbox_to_anchor=(1.05, 1), loc='upper left')
 
