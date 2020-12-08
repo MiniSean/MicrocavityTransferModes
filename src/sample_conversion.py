997493d566c5be786c9776b7435e83b648cb6aac
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-12-08 12:11:10 +0100

    WIP trying to finalize piezo element displacement fitting

--- src/sample_conversion.py
+++ src/sample_conversion.py
@@ -10,14 +10,14 @@ def transmission_to_length():
     pass
 
 
-def length_to_transmission(angle: float, n_refractive: float, wavelength: float, reflectance: float) -> Callable[[float], float]:
+def length_to_transmission(wavelength: float, reflectance: float, angle: float = 0, n_refractive: float = 1) -> Callable[[float], float]:
     """
     Calculates the round-trip phase change and resonator quality factor to construct the transmission intensity
     depending on the cavity length
-    :param angle: incidence angle [radian]
-    :param n_refractive: Cavity medium refractive index
     :param wavelength: Laser wavelength in vacuum [nm]
     :param reflectance: Cavity mirrors reflectance value
+    :param angle: incidence angle [radian]
+    :param n_refractive: Cavity medium refractive index
     :return: Callable function that maps cavity length [nm] to transmission value (0, 1]
     """
     # length: float, :param length: Cavity length [nm] (TODO: Might include penetration depth in the future)
@@ -39,19 +39,25 @@ def quality_factor(reflectance: float) -> float:
 
 
 # Piezoelectric transformations
-def voltage_to_length(max_length: float, slope: float, offset: float) -> Callable[[float], float]:
-    """
-    Estimates the piezo voltage to length by a third order polynomial
-    :param max_length: Sigmoid cap
-    :param slope: Sigmoid slope
-    :param offset: Temporal shift (5V at 10V max)
-    :return: Sigmoid displacement function
-    """
-    def sigmoid(v: float) -> float:
-        return max_length / (1 + np.exp(-slope * (v - offset)))
+def voltage_to_length(a: float, b: float = 0, c: float = 0) -> Callable[[float], float]:
 
-    def map_linear(v: float) -> float:
-        return (v / (2 * offset) - (0.003 * v**2)) * max_length
+    def map_exponential(v: float) -> float:
+        return a * v + b*(np.exp(-c * v) - 1)
+
+    # def map_linear(v: float) -> float:
+    #     return a * (v**2) + b * v + c
+
+    # """
+    # Estimates the piezo voltage to length by a third order polynomial
+    # (max_length: float, slope: float, offset: float) -> Callable[[float], float]:
+    # :param max_length: Sigmoid cap
+    # :param slope: Sigmoid slope
+    # :param offset: Temporal shift (5V at 10V max)
+    # :return: Sigmoid displacement function
+    # """
+    # def sigmoid(v: float) -> float:
+    #     return max_length / (1 + np.exp(-slope * (v - offset)))
+
     # """
     # Estimates the piezo voltage to length by a third order polynomial
     # (coef_con: float, coef_lin: float, coef_qua: float, coef_cub: float) -> Callable[[float], float]:
@@ -64,7 +70,7 @@ def voltage_to_length(max_length: float, slope: float, offset: float) -> Callabl
     # def map_function(v: float) -> float:
     #     return coef_cub * v ** 3 + coef_qua * v ** 2 + coef_lin * v + coef_con
 
-    return map_linear
+    return map_exponential
 
 
 def length_to_voltage():
@@ -79,9 +85,8 @@ def fit_voltage_to_distance(voltage_array: np.ndarray, reference_transmission_ar
     #     return A + B / (1 + C * np.cos(np.pi * 2 / (wl / 2) * (b1 * voltage + b2 + b1 * b3 * voltage ** 2)))
 
     wavelength = 794  # nm
-    half_max_voltage = 5  # [V]
 
-    def fit_function(length: np.ndarray, angle: float, n_refractive: float, reflectance: float) -> Iterator[float]:
+    def fit_function(voltage: np.ndarray, reflectance: float, L0: float, lin_coef: float, exp_coef: float, meas_offset: float) -> Iterator[float]:
         """
         Fit function that combines the piezo voltage to displacement function and the Fabry-Perot resonance length to transmission function.
         (voltage: np.ndarray, angle: float, n_refractive: float, reflectance: float, max_length: float, slope: float, offset: float) -> Iterator[float]:
@@ -94,35 +99,49 @@ def fit_voltage_to_distance(voltage_array: np.ndarray, reference_transmission_ar
         :param offset: Temporal shift
         :return: Transmission Output
         """
-        # displacement_calculator = voltage_to_length(max_length=max_length * wavelength, slope=slope, offset=half_max_voltage + offset)
-        transmission_calculator = length_to_transmission(angle=angle, n_refractive=n_refractive, wavelength=wavelength, reflectance=reflectance)
-        # length = np.fromiter(map(displacement_calculator, voltage), dtype=np.float)
-        return np.fromiter(map(transmission_calculator, length), dtype=np.float)  # map(displacement_calculator, voltage)
+        cavity_length = lin_coef * voltage + (np.exp(-voltage * exp_coef) - 1)  # Exponential
+        # cavity_length = a * voltage  # Linear
+        phase_change = (4. * np.pi / wavelength) * (L0 - cavity_length)
+        q_factor = (4. * reflectance) / (1 - reflectance)**2
+        transmission = 1. / (1 + q_factor * np.sin(0.5 * phase_change)**2)
+        return transmission + meas_offset
+        #
+        # displacement_calculator = voltage_to_length(a=a)
+        # transmission_calculator = length_to_transmission(wavelength=wavelength, reflectance=reflectance)
+        # length = L0 - np.fromiter(map(displacement_calculator, voltage), dtype=np.float)
+        # return np.fromiter(map(transmission_calculator, length), dtype=np.float)
 
-    initial_condition = [0., 1., .7]  #, 7., 1., 0.]  # Angle, n, R, max_len, slope  # [300, 600, -0.03, 0.4, 0.07, -0.7]
-    length_array = np.fromiter(map(voltage_to_length(max_length=7 * wavelength, slope=.5, offset=5.), voltage_array), dtype=np.float)  # Temp
-    popt, pvoc = curve_fit(f=fit_function, xdata=length_array, ydata=reference_transmission_array, p0=initial_condition)
+    initial_condition = [.5, 7.25 * wavelength, 0.5 * wavelength, 1., 0.]
+    bounds = ([0.0, 0, 0, -10., 0.], [1.0, 10 * wavelength, 1.5 * wavelength, 10., 1.])  # (lower, upper) bounds
+    popt, pvoc = curve_fit(f=fit_function, xdata=voltage_array, ydata=reference_transmission_array, p0=initial_condition, bounds=bounds)
     # Temp
     # yout = fit_function(voltage_array, popt[0], popt[1], popt[2], popt[3], popt[4], popt[5])
-    yout = fit_function(voltage_array, popt[0], popt[1], popt[2])
+    yout = fit_function(voltage_array, popt[0], popt[1], popt[2], popt[3], popt[4])
     plt.plot(voltage_array, reference_transmission_array)
     plt.plot(voltage_array, yout)
-    plt.show()
     return popt
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
+    from src.import_data import import_npy
+    file_samp = 'samples_1s_10V_rate1300000.0'
+    filename_base = 'transrefl_tisaph_1s_10V_PMT4_rate1300000.0'
+
+    data_array = import_npy(filename_base)[0]
+    sample = import_npy(file_samp)
     # Test length to transmission function
     w = 794
     max_len = 5*w
-    map_func = length_to_transmission(angle=0., n_refractive=1, wavelength=w, reflectance=.9)
-    map_piezo = voltage_to_length(max_length=max_len, slope=.5, offset=5.)
+    map_func = length_to_transmission(angle=0., n_refractive=1, wavelength=w, reflectance=.5)
+    map_piezo = voltage_to_length(a=100., b=50, c=300)
 
     x = np.linspace(0, max_len, 10000)
     p = np.linspace(0, 10, 10000)
-    y = np.fromiter(map(map_func, map(map_piezo, p)), dtype=np.float)
-    q = np.fromiter(map(map_piezo, p), dtype=np.float) / max_len
+    distance = 7.25*w - np.fromiter(map(map_piezo, p), dtype=np.float)
+    y = np.fromiter(map(map_func, distance), dtype=np.float)
+    q = distance / max_len
     plt.plot(p, y)
     plt.plot(p, q)
+    plt.plot(sample, data_array)
     plt.show()
