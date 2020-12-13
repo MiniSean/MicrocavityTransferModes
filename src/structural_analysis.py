42f5f7229ff4feaa32ebf2f4ab8fad30fa5be186
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-12-13 21:53:54 +0100

    Updated analysis to include prediction for sample wavelength

--- src/structural_analysis.py
+++ src/structural_analysis.py
@@ -1,7 +1,8 @@
+import numpy as np
 from typing import Optional, List
 from src.import_data import SyncMeasData
 from src.peak_identifier import identify_peaks
-from src.peak_relation import LabeledPeakCluster, flatten_clusters
+from src.peak_relation import LabeledPeakCluster, flatten_clusters, get_converted_measurement_data
 from src.peak_normalization import NormalizedPeakCollection
 
 
@@ -12,7 +13,7 @@ class MeasurementAnalysis:
         self._samp_file = samp_file
         self._scan_file = scan_file
         # Construct synchronized measurement object
-        self._meas_data = SyncMeasData(meas_file=meas_file, samp_file=samp_file, scan_file=scan_file)
+        self._meas_data = get_converted_measurement_data(SyncMeasData(meas_file=meas_file, samp_file=samp_file, scan_file=scan_file))
         self._peak_collection = NormalizedPeakCollection(identify_peaks(meas_data=self._meas_data))
 
     def __str__(self):
@@ -20,11 +21,16 @@ class MeasurementAnalysis:
         result += f'\n\n[File Info]:\nMeasurement: {self._meas_file}\nSampling: {self._samp_file}\nReference: {self._scan_file}'
         result += f'\n\n[Mode Identification Info]:\n{self.get_mode_info}'
         result += f'\n\n[Peak Identification Info]:\n{self.get_peak_info}'
+        result += f'\n\n[Physical Info]:\n{self.get_physical_info}'
         return result
 
     @property
     def get_all_mode_clusters(self) -> List[LabeledPeakCluster]:
         return self._peak_collection.get_labeled_clusters(long_mode=None, trans_mode=None)
+
+    @property
+    def get_only_fundamental_clusters(self) -> List[LabeledPeakCluster]:
+        return list(filter(lambda cluster: cluster.get_transverse_mode_id == 0, self.get_all_mode_clusters))
 
     @property
     def get_only_transverse_clusters(self) -> List[LabeledPeakCluster]:
@@ -51,6 +57,16 @@ class MeasurementAnalysis:
             _trans_cluster = self._peak_collection.get_labeled_clusters(long_mode=None, trans_mode=_trans_mode)
             _average_peaks_per_trans_mode = len(flatten_clusters(data=_trans_cluster)) / len(_trans_cluster)
             result += f'\n(m + n = {_trans_mode}) Average peaks: {round(_average_peaks_per_trans_mode, 2)}'
+        return result
+
+    @property
+    def get_physical_info(self):
+        _fundamental_only_cluster = self.get_only_fundamental_clusters
+        _fundamental_peak_distances = [abs(_fundamental_only_cluster[i].get_avg_x - _fundamental_only_cluster[i + 1].get_avg_x) for i in range(len(_fundamental_only_cluster) - 1)]
+        avg_peak_dist = np.mean(_fundamental_peak_distances)
+        std_peak_dist = np.std(_fundamental_peak_distances)
+        result = f'Average fundamental peak distance: \u03BB/2 = {round(avg_peak_dist, 2)} [nm] (\u03BB = {round(2 * avg_peak_dist, 2)} \u00B1 {round(2 * std_peak_dist, 2)} [nm])'
+        result += f'\nPredicted starting mode: q* = q - {round(_fundamental_only_cluster[0].get_avg_x / avg_peak_dist)}'
         return result
 
 
