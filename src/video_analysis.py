96035d9930d3b60339b1541e28540c4add5d76ee
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-12-16 17:07:37 +0100

    Cleanup analysis function. Added transmission spectrum visualisation

--- src/video_analysis.py
+++ src/video_analysis.py
@@ -43,7 +43,7 @@ def exclude_file_extension(full_path: str) -> str:
     return os.path.splitext(full_path)[0]
 
 
-def export_video_intensity(filename: str):
+def export_video_intensity(filename: str, update_capture_images: bool):
     folder_name = os.path.join(ICAP_DIR, f'FrameFolder_{filename}')
     capture = import_cv2(filename=filename)
     ret, frame = capture.read()
@@ -72,7 +72,7 @@ def export_video_intensity(filename: str):
 
     # Create data object
     data_class = SyncMeasData(data_array=data_array, samp_array=np.arange(len(storage)))
-    peak_collection = identify_peak_dirty(meas_data=data_class, cutoff=0.0)  # TODO: Hardcoded peak identification prominence
+    peak_collection = identify_peak_dirty(meas_data=data_class, cutoff=0.5)  # TODO: Hardcoded peak identification prominence
     print(f'Number of high intensity peaks detected: {len(peak_collection)}')
     for i, peak_data in enumerate(peak_collection):
         if i > 1000:  # safety break
@@ -84,16 +84,17 @@ def export_video_intensity(filename: str):
     plt.yscale('log')
     plt.show()
 
-    # Continue processing images
-    for peak in tqdm(peak_collection, desc=f'Retrieve prominent frames'):
-        i = int(peak.get_x)
-        # # Analyse frame
-        capture.set(cv2.CAP_PROP_POS_FRAMES, i+1)
-        _, frame_i = capture.read()
-        plt.imshow(frame_i / frame_i.max())
-        frame_name = f'frame_{str(i)}'
-        export_plt(plot_obj=plt, filename=frame_name, filepath=folder_name)
-        plt.close()
+    if update_capture_images:
+        # Continue processing images
+        for peak in tqdm(peak_collection, desc=f'Retrieve prominent frames'):
+            i = int(peak.get_x)
+            # # Analyse frame
+            capture.set(cv2.CAP_PROP_POS_FRAMES, i+1)
+            _, frame_i = capture.read()
+            plt.imshow(frame_i / frame_i.max())
+            frame_name = f'frame_{str(i)}'
+            export_plt(plot_obj=plt, filename=frame_name, filepath=folder_name)
+            plt.close()
     # Closing
     capture.release()
     cv2.destroyAllWindows()
@@ -101,5 +102,6 @@ def export_video_intensity(filename: str):
 
 if __name__ == '__main__':
 
-    filename = '07-12_800sec_mode_scanning_15FPS'  # 'test_01' 'pol000_exp005_gain800'
-    export_video_intensity(filename=filename)
+    # filename = '07-12_800sec_mode_scanning_15FPS'  # 'test_01'
+    filename = 'pol040_exp005_gain800'
+    export_video_intensity(filename=filename, update_capture_images=False)
