90e2e2a8a0471fc754711b0a3b7c32a82d01b4df
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-12-16 11:00:36 +0100

    Updated removed dependence on arbitrary noise ceiling

--- src/peak_identifier.py
+++ src/peak_identifier.py
@@ -59,12 +59,15 @@ class PeakCollection:
 
 
 def identify_peaks(meas_data: SyncMeasData) -> PeakCollection:
-    mean_prominence = np.mean(identify_peak_prominence(meas_data)[0])  # Average peak prominence
-    peak_indices, properties = find_peaks(x=meas_data.y_data, height=identify_noise_ceiling(meas_data), prominence=mean_prominence, distance=10)  # Arbitrary distance value
+    peak_prominence = identify_peak_prominence(meas_data)[0]  # Average peak prominence
+    cutoff_prominence = np.mean(peak_prominence) + 0.3 * np.std(peak_prominence)  # TODO: Hardcoded peak prominence cutoff
+    peak_indices, properties = find_peaks(x=meas_data.y_data, prominence=cutoff_prominence, distance=10)  # Arbitrary distance value
+    # , height=identify_noise_ceiling(meas_data)
     peak_collection = PeakCollection([PeakData(data=meas_data, index=i) for i in peak_indices])
     return peak_collection
 
 
+# Legacy
 def identify_noise_ceiling(meas_data: SyncMeasData) -> float:
     mean = np.mean(meas_data.y_boundless_data)
     std = np.std(meas_data.y_boundless_data)
@@ -72,7 +75,7 @@ def identify_noise_ceiling(meas_data: SyncMeasData) -> float:
 
 
 def identify_peak_prominence(meas_data: SyncMeasData) -> Tuple[np.ndarray, np.ndarray]:
-    all_peaks_above_noise = find_peaks(x=meas_data.y_data, height=identify_noise_ceiling(meas_data))[0]
+    all_peaks_above_noise = find_peaks(x=meas_data.y_data)[0]  # , height=identify_noise_ceiling(meas_data)
     return peak_prominences(x=meas_data.y_data, peaks=all_peaks_above_noise)
 
 
