c7fa9786afc8bb2a22ca4c3c1e88f749ae37a44c
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2020-12-22 00:08:50 +0100

    Updated highlight construction Added video construction

--- src/video_analysis.py
+++ src/video_analysis.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm  # For displaying for-loop process to console
 from src.import_data import import_cv2, ICAP_DIR, SyncMeasData, import_npy
-from src.peak_identifier import identify_peak_dirty
+from src.peak_identifier import identify_peak_dirty, PeakCollection
 
 
 def export_plt(plot_obj: plt, filename: str, filepath: str) -> str:
@@ -43,7 +43,19 @@ def exclude_file_extension(full_path: str) -> str:
     return os.path.splitext(full_path)[0]
 
 
-def export_video_intensity(filename: str, update_capture_images: bool):
+def post_processing(frame: np.ndarray, base_frame: np.ndarray) -> np.ndarray:
+    """Applies non-destructive filtering"""
+    # mean_cutoff = np.mean(base_frame)  # Mean cutoff
+    # frame = ndimage.gaussian_filter(frame, 3)  # High frequency noise filter
+    #
+    # sub_threshold_indices = frame < mean_cutoff  # Apply base cutoff
+    # frame[sub_threshold_indices] = 0
+
+    frame = frame / frame.max()  # Normalization
+    return frame
+
+
+def export_video_intensity(filename: str, update_capture_images: bool, build_video: bool):
     split_directory = filename.split('\\')
     actual_filename = split_directory[-1]
     actual_filedir = '\\'.join(split_directory[0:-1])
@@ -51,7 +63,8 @@ def export_video_intensity(filename: str, update_capture_images: bool):
     capture = import_cv2(filename=filename)
     ret, frame = capture.read()
     max_frame_nr = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
-    print(f'Number of frames recorded: {max_frame_nr} ({round((max_frame_nr / 15) / 60, 2)} min)')
+    fps = capture.get(cv2.CAP_PROP_FPS)
+    print(f'Number of frames recorded: {max_frame_nr} ({round((max_frame_nr / fps) / 60, 2)} min)')
 
     # Ready data storage
     storage = np.zeros(max_frame_nr)
@@ -75,18 +88,19 @@ def export_video_intensity(filename: str, update_capture_images: bool):
 
     # Create data object
     data_class = SyncMeasData(data_array=data_array, samp_array=np.arange(len(storage)))
-    peak_collection = identify_peak_dirty(meas_data=data_class, cutoff=0.5)  # TODO: Hardcoded peak identification prominence
+    peak_collection = identify_peak_dirty(meas_data=data_class, cutoff=0.1)  # TODO: Hardcoded peak identification prominence
     print(f'Number of high intensity peaks detected: {len(peak_collection)}')
-    for i, peak_data in enumerate(peak_collection):
-        if i > 1000:  # safety break
-            break
-        if peak_data.relevant:
-            plt.plot(peak_data.get_x, peak_data.get_y, 'x', color='r', alpha=1)
+    plot_peaks(plt, peak_collection)
 
     plt.plot(data_array)
     plt.yscale('log')
     plt.show()
 
+    # Base reference frame
+    capture.set(cv2.CAP_PROP_POS_FRAMES, 1)
+    _, frame_0 = capture.read()
+
+    # Construct highlight capture images
     if update_capture_images:
         # Continue processing images
         for peak in tqdm(peak_collection, desc=f'Retrieve prominent frames'):
@@ -94,17 +108,85 @@ def export_video_intensity(filename: str, update_capture_images: bool):
             # # Analyse frame
             capture.set(cv2.CAP_PROP_POS_FRAMES, i+1)
             _, frame_i = capture.read()
-            plt.imshow(frame_i / frame_i.max())
+            frame_i = post_processing(frame=frame_i, base_frame=frame_0)
+
+            fig, axis = plt.subplots()
+            axis.get_xaxis().set_visible(False)  # Hide tick labels
+            axis.get_yaxis().set_visible(False)  # Hide tick labels
+            inset_axis = add_inset_spectrum(figure=fig)
+            axis.imshow(frame_i)
+            # Create navigation spectrum inset
+            set_inset_spectrum(axis=inset_axis, data=data_array, current_index=i, peak_collection=peak_collection)
+
+            # plt.show()  # Temp
             frame_name = f'frame_{str(i)}'
-            export_plt(plot_obj=plt, filename=frame_name, filepath=folder_name)
+            plot_filepath = export_plt(plot_obj=plt, filename=frame_name, filepath=folder_name)
             plt.close()
+
+    # Create video
+    if build_video:
+        # output_name = f'Highlight_test.avi'  # {actual_filename}
+        # cmd = f'ffmpeg -framerate {fps} -pattern_type glob -i {os.path.join(folder_name, "*.png")} {output_name}'
+        # process = subprocess.Popen(cmd)
+        size = (frame_0.shape[1], frame_0.shape[0])
+        output_filepath = os.path.join(folder_name, f'Highlight_{actual_filename}.avi')
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')  # cv2.VideoWriter_fourcc(*'mp4v')
+        video = cv2.VideoWriter(output_filepath, fourcc, fps, size)
+        # Buffer image file path
+        plot_image_buffer = [os.path.join(folder_name, f'frame_{str(int(peak.get_x))}.png') for peak in peak_collection]
+        for path in tqdm(reversed(plot_image_buffer), desc=f'Construct highlight video'):
+            frame = cv2.imread(filename=path)
+            video.write(frame)  # Append video
+        video.release()
+
     # Closing
     capture.release()
     cv2.destroyAllWindows()
 
 
+def construct_video(filename: str):
+    split_directory = filename.split('\\')
+    actual_filename = split_directory[-1]
+    actual_filedir = '\\'.join(split_directory[0:-1])
+    folder_name = os.path.join(os.path.join(ICAP_DIR, actual_filedir), f'FrameFolder_{actual_filename}')
+
+    # Fetch images
+    for img_name in glob.glob(os.path.join(folder_name, '*.png')):
+        print(img_name)
+
+
+def plot_peaks(axis: plt.axis, collection: PeakCollection) -> plt.axis:
+    for i, peak_data in enumerate(collection):
+        if i > 1000:  # safety break
+            break
+        if peak_data.relevant:
+            axis.plot(peak_data.get_x, peak_data.get_y, 'x', color='r', alpha=1)
+    return axis
+
+
+def add_inset_spectrum(figure: plt.Figure) -> plt.axis:
+    rect_transform = [.7, .7, .25, .25]  # (0,0 is bottom left; 1,1 is top right)
+    _axis = figure.add_axes(rect_transform)
+    _axis.get_xaxis().set_visible(False)  # Hide tick labels
+    _axis.get_yaxis().set_visible(False)  # Hide tick labels
+    _axis.set_yscale('log')  # Set log scale
+    return _axis
+
+
+def set_inset_spectrum(axis: plt.axis, data: np.ndarray, current_index: int, peak_collection: PeakCollection) -> plt.axis:
+    axis.plot(data)  # Show intensity spectrum
+    axis = plot_peaks(axis=axis, collection=peak_collection)  # Show peaks
+    axis.axvline(x=current_index, color='r')
+    _margin = 50
+    axis.set_xlim([max(0, current_index - _margin), min(len(data) - 1, current_index + _margin)])
+    # axis.set_ylim([max(0.001, np.min(data)), np.max(data)])
+    return axis
+
+
 if __name__ == '__main__':
 
-    # filename = '07-12_800sec_mode_scanning_15FPS'  # 'test_01'
-    filename = 'pol040_exp005_gain800'
-    export_video_intensity(filename=filename, update_capture_images=False)
+    # filename = 'test_01'
+    # filename = '07-12_800sec_mode_scanning_15FPS'
+    # filename = 'pol000_exp005_gain800'
+    filename = 'pol000_fps0375_gain1000_01HIGHQUALITY'
+    export_video_intensity(filename=filename, update_capture_images=False, build_video=True)
