e7e0b17081418f281e8d8de8dfcf4ddc157b3118
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2021-01-31 11:54:09 +0100

    Added ability to perform entire frame sequence sweep

--- src/video_analysis.py
+++ src/video_analysis.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm  # For displaying for-loop process to console
 from src.import_data import import_cv2, ICAP_DIR, SyncMeasData, import_npy
-from src.peak_identifier import identify_peak_dirty, PeakCollection
+from src.peak_identifier import identify_peak_dirty, identify_peaks, PeakCollection
 from src.peak_relation import LabeledPeakCollection
 
 
@@ -51,12 +51,12 @@ def post_processing(frame: np.ndarray, base_frame: np.ndarray) -> np.ndarray:
     #
     # sub_threshold_indices = frame < mean_cutoff  # Apply base cutoff
     # frame[sub_threshold_indices] = 0
-
+    frame = np.clip(frame - base_frame, 0, 225)
     frame = frame / frame.max()  # Normalization
     return frame
 
 
-def export_video_intensity(filename: str, update_capture_images: bool, build_video: bool):
+def export_video_intensity(filename: str, update_capture_images: bool, build_video: bool, full_sweep: bool):
     split_directory = filename.split('\\')
     actual_filename = split_directory[-1]
     actual_filedir = '\\'.join(split_directory[0:-1])
@@ -82,6 +82,12 @@ def export_video_intensity(filename: str, update_capture_images: bool, build_vid
         storage = np.asarray(storage)
         export_npy(npy_object=storage, filename=storage_filename, filepath=folder_name)
 
+    # Background filter
+    filter_start_frame = 20
+    filter_range = 100
+    full_capt_start = 2450  # Temp
+    full_capt_end = 2530  # Temp
+
     data_array = np.nan_to_num(storage)
     data_array /= np.max(np.abs(data_array), axis=0)
     sub_threshold_indices = data_array < 0.0  # TODO: Hardcoded (normalized) intensity threshold
@@ -89,43 +95,84 @@ def export_video_intensity(filename: str, update_capture_images: bool, build_vid
 
     # Create data object
     data_class = SyncMeasData(data_array=data_array, samp_array=np.arange(len(storage)))
-    peak_collection = identify_peak_dirty(meas_data=data_class, cutoff=0.1)  # TODO: Hardcoded peak identification prominence
-    peak_collection = LabeledPeakCollection(transmission_peak_collection=peak_collection)
+    peak_collection = identify_peak_dirty(meas_data=data_class, cutoff=0.0)  # TODO: Hardcoded peak identification prominence
+    peak_collection = PeakCollection([peak for peak in peak_collection if peak.get_y > 0.34])
+    # peak_collection = LabeledPeakCollection(transmission_peak_collection=peak_collection)
     print(f'Number of high intensity peaks detected: {len(peak_collection)}')
     plot_peaks(plt, peak_collection)
 
     plt.plot(data_array)
+    plt.axvline(x=filter_start_frame, color='r')
+    plt.axvline(x=filter_start_frame + filter_range, color='r')
+    plt.axvline(x=full_capt_start, color='darkorange')  # Temp
+    plt.axvline(x=full_capt_end, color='darkorange')  # Temp
     plt.yscale('log')
     plt.show()
 
     # Base reference frame
     capture.set(cv2.CAP_PROP_POS_FRAMES, 1)
     _, frame_0 = capture.read()
+    frame_0 = np.ndarray(shape=frame_0.shape)
 
     # Construct highlight capture images
     if update_capture_images:
-        # Continue processing images
-        for peak in tqdm(peak_collection, desc=f'Retrieve prominent frames'):
-            i = int(peak.get_x)
+        # Get average background for subtraction
+        for i in range(filter_start_frame, filter_start_frame + filter_range):
             # # Analyse frame
             capture.set(cv2.CAP_PROP_POS_FRAMES, i+1)
             _, frame_i = capture.read()
-            frame_i = post_processing(frame=frame_i, base_frame=frame_0)
+            frame_0 += frame_i
+        frame_0 = frame_0 / filter_range  # Normalize
 
-            fig, axis = plt.subplots()
-            axis.get_xaxis().set_visible(False)  # Hide tick labels
-            axis.get_yaxis().set_visible(False)  # Hide tick labels
-            inset_axis = add_inset_spectrum(figure=fig)
-            track_axis = add_track_spectrum(figure=fig)
-            axis.imshow(frame_i)
-            # Create navigation spectrum inset
-            set_inset_spectrum(axis=inset_axis, data=data_array, current_index=i, peak_collection=peak_collection)
-            set_track_spectrum(axis=track_axis, data=data_array, current_index=i)
+        if full_sweep:
+            # Get all frames in range
+            for i in tqdm(range(full_capt_start, full_capt_end), desc=f'Retrieve full range of frames'):
+                if i % 2 == 0:
+                    continue
+                # # Analyse frame
+                capture.set(cv2.CAP_PROP_POS_FRAMES, i+1)
+                _, frame_i = capture.read()
+                frame_i = post_processing(frame=frame_i, base_frame=frame_0)
 
-            # plt.show()  # Temp
-            frame_name = f'frame_{str(i)}'
-            plot_filepath = export_plt(plot_obj=plt, filename=frame_name, filepath=folder_name)
-            plt.close()
+                fig, axis = plt.subplots()
+                axis.get_xaxis().set_visible(False)  # Hide tick labels
+                axis.get_yaxis().set_visible(False)  # Hide tick labels
+                inset_axis = add_inset_spectrum(figure=fig)
+                track_axis = add_track_spectrum(figure=fig)
+                axis.imshow(frame_i)
+                # Create navigation spectrum inset
+                set_inset_spectrum(axis=inset_axis, data=data_array, current_index=i, peak_collection=peak_collection)
+                set_track_spectrum(axis=track_axis, data=data_array, current_index=i)
+
+                # plt.show()  # Temp
+                frame_name = f'frame_{str(i)}'  #_bright
+                plot_filepath = export_plt(plot_obj=plt, filename=frame_name, filepath=folder_name)
+                plt.close()
+        else:
+            # Continue processing images
+            for peak in tqdm(peak_collection, desc=f'Retrieve prominent frames'):
+                j = int(peak.get_x)
+                j_margin = 0
+                for i in range(j - j_margin, j + j_margin + 1):
+                    # Analyse frame
+                    capture.set(cv2.CAP_PROP_POS_FRAMES, i+1)
+                    _, frame_i = capture.read()
+                    frame_i = post_processing(frame=frame_i, base_frame=frame_0)
+
+                    fig, axis = plt.subplots()
+                    axis.get_xaxis().set_visible(False)  # Hide tick labels
+                    axis.get_yaxis().set_visible(False)  # Hide tick labels
+                    inset_axis = add_inset_spectrum(figure=fig)
+                    track_axis = add_track_spectrum(figure=fig)
+                    axis.imshow(frame_i)
+                    # Create navigation spectrum inset
+                    set_inset_spectrum(axis=inset_axis, data=data_array, current_index=i, peak_collection=peak_collection)
+                    set_track_spectrum(axis=track_axis, data=data_array, current_index=i)
+
+                    # plt.show()  # Temp
+                    frame_name = f'frame_{str(i)}'  #_bright
+                    plot_filepath = export_plt(plot_obj=plt, filename=frame_name, filepath=folder_name)
+                    plt.close()
 
     # Create video
     if build_video:
@@ -146,6 +193,28 @@ def export_video_intensity(filename: str, update_capture_images: bool, build_vid
     # Closing
     capture.release()
     cv2.destroyAllWindows()
+
+
+def capture_frame(capture: cv2.VideoCapture, data_array: np.ndarray, index: int, frame_0: np.ndarray, folder_name: str, peak_collection: PeakCollection):
+    # # Analyse frame
+    capture.set(cv2.CAP_PROP_POS_FRAMES, index+1)
+    _, frame_i = capture.read()
+    frame_i = post_processing(frame=frame_i, base_frame=frame_0)
+
+    fig, axis = plt.subplots()
+    axis.get_xaxis().set_visible(False)  # Hide tick labels
+    axis.get_yaxis().set_visible(False)  # Hide tick labels
+    inset_axis = add_inset_spectrum(figure=fig)
+    track_axis = add_track_spectrum(figure=fig)
+    axis.imshow(frame_i)
+    # Create navigation spectrum inset
+    set_inset_spectrum(axis=inset_axis, data=data_array, current_index=index, peak_collection=peak_collection)
+    set_track_spectrum(axis=track_axis, data=data_array, current_index=index)
+
+    # plt.show()  # Temp
+    frame_name = f'frame_{str(index)}'  #_bright
+    plot_filepath = export_plt(plot_obj=plt, filename=frame_name, filepath=folder_name)
+    plt.close()
 
 
 def construct_video(filename: str):
@@ -195,12 +264,12 @@ def set_inset_spectrum(axis: plt.axis, data: np.ndarray, current_index: int, pea
     _margin = 50
     x_lim = [max(0, current_index - _margin), min(len(data) - 1, current_index + _margin)]
     # Plot clusters
-    for cluster in peak_collection.get_clusters:
-        bound_left, bound_right = cluster.get_value_slice
-        if bound_right > x_lim[0] or bound_left < x_lim[1]:
-            axis.axvspan(bound_left, bound_right, alpha=0.5, color='green')
-        if x_lim[0] < cluster.get_avg_x < x_lim[1]:
-            axis.text(x=cluster.get_avg_x, y=text_height, s=r'$\tilde{m}$'+f'={cluster.get_transverse_mode_id}')
+    # for cluster in peak_collection.get_clusters:
+    #     bound_left, bound_right = cluster.get_value_slice
+    #     if bound_right > x_lim[0] or bound_left < x_lim[1]:
+    #         axis.axvspan(bound_left, bound_right, alpha=0.5, color='green')
+    #     if x_lim[0] < cluster.get_avg_x < x_lim[1]:
+    #         axis.text(x=cluster.get_avg_x, y=text_height, s=r'$\tilde{m}$'+f'={cluster.get_transverse_mode_id}')
     axis.axvline(x=current_index, color='r')
     axis.set_xlim(x_lim)
     return axis
@@ -221,5 +290,8 @@ if __name__ == '__main__':
     # filename = 'pol040_exp005_gain800'
     # filename = '21-12_10micromcav\\pol000_exp005_gain800_04'
     # filename = '21-12_10micromcav\\pol000_fps0375_gain1000_01HIGHQUALITY'
-    filename = '21-12_10micromcav\\pol000_fps0750_gain1000_01'
-    export_video_intensity(filename=filename, update_capture_images=True, build_video=False)
+    # filename = '21-12_10micromcav\\pol000_fps0750_gain1000_01'
+    # filename = '27-01_12micromcav\\TM03\\V683_689_G800_15FPS'
+    # filename = '27-01_12micromcav\\TM03\\V683_689_G800_15FPS_P045'
+    filename = '27-01_12micromcav\\TM03_LowQ\\V703_711_G800_15FPS_P067'
+    export_video_intensity(filename=filename, update_capture_images=True, build_video=False, full_sweep=True)
